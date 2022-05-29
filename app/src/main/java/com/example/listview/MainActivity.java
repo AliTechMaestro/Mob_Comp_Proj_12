ed3f97ef8a9fab0d4e428882cecdf34469a44e22
DELL PC <bsef19a525@pucit.edu.pk>
2022-05-29 12:54:12 +0500

    ListView with EditText (Commit 3 Done)

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
+
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText task;
+    Button btn;
 
     ArrayList<String> friendList = new ArrayList<>();
     ListView listView;
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         friendList.add("Rauf");
         friendList.add("Faisal");
@@ -30,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
                 (this, android.R.layout.simple_list_item_1,friendList);
         listView =  findViewById(R.id.listView);
         listView.setAdapter(arrayAdapter);
+
+        task = findViewById(R.id.inputText);
+        btn = findViewById(R.id.addBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                friendList.add(task.getText().toString());
+                task.setText("");
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+
 
 
 
